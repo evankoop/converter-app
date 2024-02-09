99715a0fafeb7b016cb4f247ae7aba97068e61af
evankoop <koopevan@gmail.com>
2024-02-09 11:10:52 -0700

    Testing more conditions with specific benefits

--- conversion.py
+++ conversion.py
@@ -92,8 +92,18 @@ def xml_to_csv(xml_file, csv_file):
                     for enrollment_data_item in employee_enrollments:
 
                         combined_data = {**company_data, **employee_data, **enrollment_data_item}
+
+                        benefit = enrollment_data_item.get('Benefit')
                         
-                        if voluntary_disability_data and enrollment_data_item.get('Benefit') in voluntary_disability_benefits:
+                        if voluntary_disability_data and benefit in voluntary_disability_benefits:
+                            if benefit == 'Voluntary Short-Term Disability':
+                                combined_data.update(voluntary_disability_data[0])
+                            elif benefit == 'Voluntary Long-Term Disability':
+                                combined_data.update(voluntary_disability_data[0])
+                            elif benefit == 'Short-Term Disability':
+                                combined_data.update(voluntary_disability_data[0])
+                            elif benefit == 'Long-Term Disability':
+                                combined_data.update(voluntary_disability_data[0])
                             combined_data.update(voluntary_disability_data[0])
 
                         if voluntary_life_data and enrollment_data_item.get('Benefit') in voluntary_life_benefits:
